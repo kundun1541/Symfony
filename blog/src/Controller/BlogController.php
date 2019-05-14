3b0beeebdc9533c6d46095c198a009b8af803f09
Sylvain <sylvaingrosrey@gmail.com>
2019-05-14 11:24:25 +0200

    Quete 4 update

--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -1,32 +1,36 @@
 <?php
+// src/Controller/BlogController.php
 namespace App\Controller;
-
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
+/**
+ * @Route("/blog")
+ */
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog/list/{page<\d+>}", name="blog_list")
+     * @Route("/", name="blog_index")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', ['owner' => 'Dimitri',]);
+    }
+    /**
+     * @Route("/list/{page}", requirements={"page"="\d+"}, name="blog_list")
      */
     public function list($page)
     {
         return $this->render('blog/list.html.twig', ['page' => $page]);
     }
-
     /**
-     * @Route(
-     *     "blog/show/{slug}",
-     *     name="blog_list",
-     *     )
+     * @Route("/show/{slug}", defaults={"slug"="Article Sans Titre"}, requirements={"slug"="[a-z0-9-]+"},
+     *     name="blog_show")
      */
-
     public function show($slug)
     {
-        $slugspace = str_replace("-", " ", $slug);
-        $slugspacemaj = ucwords($slugspace);
-        return $this->render('blog/show.html.twig', ['slugspacemaj' => $slugspacemaj]);
+        $slug = str_replace("-", " ", $slug);
+        $slug = ucwords($slug);
+        return $this->render('blog/show.html.twig', ['slug' => $slug]);
     }
 }
--- blog/src/Controller/DefaultController.php
+++ blog/src/Controller/DefaultController.php
@@ -1,23 +1,16 @@
 <?php
-// src/Controller/DefaultController.phpphp
+// src/Controller/DefaultController.php
 namespace App\Controller;
-
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/index", name="app_index")
+     * @Route("/", name="app_index")
      */
     public function index()
     {
-        return $this->render('index.html.twig', [
-            'owner' => 'Sylvain'
-        ]);
+        return $this->render('default.html.twig');
     }
-
 }
