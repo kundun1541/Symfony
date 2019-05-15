6b54889fc139bc47175312d8230278b50a6c8450
Sylvain <sylvaingrosrey@gmail.com>
2019-05-15 17:20:45 +0200

    Quete 7 atomique

--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -4,18 +4,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 /**
  * @Route("/blog")
  */
 class BlogController extends AbstractController
 {
+
     /**
+     * Show all row from article's entity
+     *
      * @Route("/", name="blog_index")
+     * @return Response A response instance
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('blog/index.html.twig', ['owner' => 'Sylvain',]);
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/index.html.twig',
+            ['articles' => $articles]
+        );
     }
+
     /**
      * @Route("/list/{page}", requirements={"page"="\d+"}, name="blog_list")
      */
@@ -23,14 +43,46 @@ class BlogController extends AbstractController
     {
         return $this->render('blog/list.html.twig', ['page' => $page]);
     }
+
+
     /**
-     * @Route("/show/{slug}", defaults={"slug"="Article Sans Titre"}, requirements={"slug"="[a-z0-9-]+"},
+     * Getting a article with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     *
+     * @Route("/{slug<^[a-z0-9-]+$>}",
+     *     defaults={"slug" = null},
      *     name="blog_show")
+     * @return Response A response instance
      */
-    public function show($slug)
+    public function show(?string $slug): Response
     {
-        $slug = str_replace("-", " ", $slug);
-        $slug = ucwords($slug);
-        return $this->render('blog/show.html.twig', ['slug' => $slug]);
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with ' . $slug . ' title, found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/show.html.twig',
+            [
+                'article' => $article,
+                'slug' => $slug,
+            ]
+        );
     }
 }
